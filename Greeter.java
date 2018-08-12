f02712fe02dfe4873d2819953e34795234f9fc4d
Sai Ancha <sai.ancha194@gmail.com>
2018-08-12 22:29:02 +1200

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,6 +17,9 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
+    if (args.length < 3) {
+      System.exit(1);
+    }
     String language = args[0];
     String country = args[1];
     String name = args[2];
